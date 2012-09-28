e9dcffd70bf2c4992d15271962ee804268c1ad6e
Raymond T. Hightower <rth@wisdomgroup.com>
2012-09-28 16:34:22 -0400

    sixth iteration complete

--- conway.rb
+++ conway.rb
@@ -1,11 +1,39 @@
 class Board
 
-  def initialize
+  def initialize(live_cells=nil)
+
+    @live_cells ||= live_cells
     
   end
 
   def cells
     @cells ||= Array.new
+  end
+
+end
+
+class Cell
+  attr_reader :x_coord, :y_coord
+  def initialize(x_coord=nil, y_coord=nil, state=nil)
+    @x_coord, @y_coord, @state = x_coord, y_coord, state
+    
+  end
+
+  def alive?
+    @state
+    
+  end
+
+  def neighbors
+    @neighbors
+  end
+
+  def remove_neighbor(other)
+
+  end
+
+  def add_neighbor(other)
+
   end
 
 end
--- conway_test.rb
+++ conway_test.rb
@@ -22,6 +22,29 @@ describe Cell do
   it "is either dead or alive" do
     @cell = Cell.new
     @cell.wont_be_nil
+  end
+
+  it "should be alive if initialized with true" do
+    @cell = Cell.new(0,0,true)
+    @cell.alive?.must_equal true
+  end
+
+  it "should know its position" do
+    x_coord = 3
+    y_coord = 5
+    @cell = Cell.new(x_coord, y_coord)
+
+    @cell.x_coord.must_equal 3
     
   end
+
+  it "should know who it's neighbors are" do
+    @cell1 = Cell.new(1, 1)
+    @cell2 = Cell.new(1, 2)
+    @cell3 = Cell.new(2, 1)
+    @board = Board.new([@cell1, @cell2, @cell3])
+    @cell1.neighbors.must_be == [@cell2, @cell3]
+  end
+
+
 end
